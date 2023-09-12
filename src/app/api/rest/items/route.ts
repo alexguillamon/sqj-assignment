9988889291fdd7bb5a4afa3abdbb1fb90db231cb
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 10:40:32 -0500

    implement rest /items routes

---
+++ src/app/api/rest/items/route.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+import { fromZodError } from "zod-validation-error";
+import { db, schema } from "~/backend/db";
+
+export async function GET(request: Request) {
+  try {
+    const items = await db.query.items.findMany();
+    return new Response(JSON.stringify(items));
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ message: error.message }), {
+        status: 500,
+      });
+    }
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    // Parse the JSON body into a JavaScript object
+    const data = await request.json().catch((error) => {
+      throw new Error("Invalid JSON");
+    });
+    // Validate the data against the schema
+    const item = schema.insertItemsSchema.parse(data);
+
+    // Insert the data into the database
+    const result = await db.insert(schema.items).values(data).returning();
+    // Return the inserted data
+    return new Response(JSON.stringify(result));
+  } catch (error) {
+    // Handle validation errors
+    if (error instanceof z.ZodError) {
+      return new Response(
+        JSON.stringify({ message: fromZodError(error).toString() }),
+        {
+          status: 400,
+        }
+      );
+    }
+    // Handle other errors
+    if (error instanceof Error) {
+      // Handle invalid JSON
+      if (error.message === "Invalid JSON") {
+        return new Response(JSON.stringify({ message: error.message }), {
+          status: 400,
+        });
+      }
+      // Catch-all db errors
+      return new Response(JSON.stringify({ message: error.message }), {
+        status: 500,
+      });
+    }
+    // Catch-all
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}

c0d5d67247752c83bd2d9485f882440e449f651c
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 12:32:42 -0500

    implement put and delete

--- src/app/api/rest/items/[id]/route.ts
+++ src/app/api/rest/items/[id]/route.ts
@@ -1,20 +1,25 @@
-import { db, eq, items } from "~/backend/db";
+import { z } from "zod";
+import { fromZodError } from "zod-validation-error";
+import { db, eq, insertItemsSchema, items } from "~/backend/db";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: number } }
 ) {
   try {
+    // Find the item with the given ID
     const item = await db.query.items.findFirst({
       where: eq(items.id, params.id),
     });
+    // If no item was found, return a 404
     if (!item) {
       return new Response(JSON.stringify({ message: "Not Found" }), {
         status: 404,
       });
     }
-    return new Response(JSON.stringify(item));
+    return new Response(JSON.stringify({ data: item }));
   } catch (error) {
+    // Catch-all
     if (error instanceof Error) {
       return new Response(JSON.stringify({ message: error.message }), {
         status: 500,
@@ -28,10 +33,89 @@ export async function GET(
 
 export async function PUT(
   request: Request,
-  { params }: { params: { id: string } }
-) {}
+  { params }: { params: { id: number } }
+) {
+  try {
+    // Parse the JSON body into a JavaScript object
+    const data = await request.json().catch((error) => {
+      throw new Error("Invalid JSON");
+    });
+
+    // Validate the data against the schema (You can create a separate schema for updates if needed)
+    const item = insertItemsSchema.parse(data);
+
+    // Update the record in the database based on the ID
+    const result = await db
+      .update(items)
+      .set(item)
+      .where(eq(items.id, params.id))
+      .returning();
+
+    if (result.length > 0) {
+      return new Response(JSON.stringify({ data: result[0] }));
+    } else {
+      return new Response(JSON.stringify({ message: "Item not found" }), {
+        status: 404,
+      });
+    }
+  } catch (error) {
+    // Handle validation errors
+    if (error instanceof z.ZodError) {
+      return new Response(
+        JSON.stringify({ message: fromZodError(error).toString() }),
+        {
+          status: 400,
+        }
+      );
+    }
+    // Handle other errors
+    if (error instanceof Error) {
+      // Handle invalid JSON
+      if (error.message === "Invalid JSON") {
+        return new Response(JSON.stringify({ message: error.message }), {
+          status: 400,
+        });
+      }
+      // Catch-all for other errors (e.g., database errors)
+      return new Response(JSON.stringify({ message: error.message }), {
+        status: 500,
+      });
+    }
+    // Catch-all
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
-) {}
+  { params }: { params: { id: number } }
+) {
+  try {
+    const result = await db
+      .delete(items)
+      .where(eq(items.id, params.id))
+      .returning();
+
+    if (result.length > 0) {
+      return new Response(
+        JSON.stringify({ message: "success", data: result[0] })
+      );
+    } else {
+      return new Response(JSON.stringify({ message: "Item not found" }), {
+        status: 404,
+      });
+    }
+  } catch (error) {
+    // Catch-all
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ message: error.message }), {
+        status: 500,
+      });
+    }
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}

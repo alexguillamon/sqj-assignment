a1b73535b1bf48941179dad378be297003a76f95
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 17:14:50 -0500

    add title to collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ export default async function HomePage() {
   return (
     <main className="flex h-full flex-col items-center bg-white text-black">
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 md:max-w-7xl md:px-8">
+        <h1 className="text-4xl font-bold text-center pt-10">
+          The Best Collection
+        </h1>
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-10 md:max-w-7xl md:px-8">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-8">
             {items.map((product) => (
               <div key={product.id} className="group relative">

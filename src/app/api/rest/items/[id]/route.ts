87f8a8a5e8157538550cafa1139d66d0a0bb848f
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 10:49:49 -0500

    implement GET /items/:id route

--- src/app/api/rest/items/[id]/route.ts
+++ src/app/api/rest/items/[id]/route.ts
@@ -1,7 +1,30 @@
+import { db, eq, items } from "~/backend/db";
+
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
-) {}
+  { params }: { params: { id: number } }
+) {
+  try {
+    const item = await db.query.items.findFirst({
+      where: eq(items.id, params.id),
+    });
+    if (!item) {
+      return new Response(JSON.stringify({ message: "Not Found" }), {
+        status: 404,
+      });
+    }
+    return new Response(JSON.stringify(item));
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ message: error.message }), {
+        status: 500,
+      });
+    }
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
 
 export async function PUT(
   request: Request,

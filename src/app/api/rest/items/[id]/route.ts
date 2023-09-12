68d99c2aed74ea7d4eca0a0e0590742b369eae0d
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 10:41:19 -0500

    add rest /items/:id skeleton routes

---
+++ src/app/api/rest/items/[id]/route.ts
@@ -0,0 +1,14 @@
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {}

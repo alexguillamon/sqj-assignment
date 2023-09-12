a8ba458ff2e5c857911234081cd23be86e1a000f
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 09:00:29 -0500

    debug db push script

--- src/backend/db/drizzle.config.ts
+++ src/backend/db/drizzle.config.ts
@@ -1,17 +1,15 @@
 import * as dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
 
-dotenv.config({
-  path: "../../../.env",
-});
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
 export default {
-  schema: "./schema",
-  driver: 'pg',
+  schema: "./src/backend/db/schema",
+  driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },

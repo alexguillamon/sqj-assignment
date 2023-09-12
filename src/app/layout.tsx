5ad8f160f8fb751cc6c8c36251e12a84b1161a37
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 13:11:20 -0500

    implement header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
+import Header from "~/components/Header";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "TechStore",
     description: "TechStore's amazing tech store",
-    url: "",
+    url: "https://squadjobs.com/",
     siteName: "TechStore",
   },
 };
@@ -23,8 +24,23 @@ export default function Layout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={["font-sans", fontSans.variable].join(" ")}>
+        <Header />
         {props.children}
       </body>
     </html>
   );
 }
+
+// Todo: Add a footer
+// todo: add detail route
+// todo: add admin route
+// todo: add items view
+// todo: add update item view
+// todo: add insert item view
+// todo: add delete item button
+// todo: add update/insert/delete success/failure banner
+// todo: write readme
+// todo: add unit tests
+// todo: change color to match squadjobs
+// extra: add cart functionality
+// extra: add auth function on admin page

66da14993c67ddfb929962eaf026f87453a02cac
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-12 13:11:46 -0500

    implement product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,45 @@
+import { db } from "~/backend/db";
+
 export const runtime = "edge";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const items = await db.query.items.findMany();
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Starter Skeleton
-        </h1>
+    <main className="flex h-full flex-col items-center bg-white text-black">
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 md:max-w-7xl md:px-8">
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-8">
+            {items.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 md:aspect-none group-hover:opacity-75 md:h-90">
+                  <img
+                    src={
+                      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    }
+                    alt={"Front of men's Basic Tee in black."}
+                    className="h-full w-full object-cover object-center md:h-full md:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={"#"}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {product.description}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    {product.price}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );

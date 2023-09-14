9957112c76276a6ac898bb614405628f309df312
alexguillamon <22422193+alexguillamon@users.noreply.github.com>
2023-09-14 17:53:00 -0500

    extract product grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import { db } from "~/backend/db";
+import ProductGrid from "../components/ProductGrid";
 
 export default async function HomePage() {
   const items = await db.query.items.findMany();
@@ -11,44 +10,7 @@ export default async function HomePage() {
           The Best Collection
         </h1>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-10 md:max-w-7xl md:px-8">
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-8">
-            {items.map((item) => (
-              <div key={item.id} className="group relative">
-                <div className="aspect-h-3 aspect-w-2 w-full overflow-hidden rounded-md bg-gray-200  group-hover:opacity-75 lg:h-96">
-                  <Image
-                    src={item.imageUrl}
-                    alt={item.name}
-                    height={500}
-                    width={350}
-                    className="h-full w-full object-cover object-center md:h-full md:w-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between relative w-full">
-                  <div className="relative max-w-[75%]">
-                    <h3 className="text-sm text-gray-700 truncate ">
-                      <Link href={"/" + item.id}>
-                        {/* <span
-                          aria-hidden="true"
-                          className="absolute inset-0 "
-                        /> */}
-                        {item.name}
-                      </Link>
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500 truncate">
-                      {item.description}
-                    </p>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900 truncate min-w-[25%] text-end">
-                    $
-                    {parseFloat(item.price).toLocaleString("en-US", {
-                      minimumFractionDigits: 2,
-                      maximumFractionDigits: 2,
-                    })}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+          <ProductGrid items={items} />
         </div>
       </div>
     </main>
---
+++ src/components/ProductGrid.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Item } from "~/backend/db";
+
+export default async function ProductGrid({ items }: { items: Item[] }) {
+  return (
+    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:gap-x-8">
+      {items.map((item) => (
+        <div key={item.id} className="group relative">
+          <div className="aspect-h-3 aspect-w-2 w-full overflow-hidden rounded-md bg-gray-200  group-hover:opacity-75 lg:h-96">
+            <Image
+              src={item.imageUrl}
+              alt={item.name}
+              height={500}
+              width={350}
+              className="h-full w-full object-cover object-center md:h-full md:w-full"
+            />
+          </div>
+          <div className="mt-4 flex justify-between relative w-full">
+            <div className="relative max-w-[75%]">
+              <h3 className="text-sm text-gray-700 truncate ">
+                <Link href={"/" + item.id}>
+                  {/* <span
+                          aria-hidden="true"
+                          className="absolute inset-0 "
+                        /> */}
+                  {item.name}
+                </Link>
+              </h3>
+              <p className="mt-1 text-sm text-gray-500 truncate">
+                {item.description}
+              </p>
+            </div>
+            <p className="text-sm font-medium text-gray-900 truncate min-w-[25%] text-end">
+              $
+              {parseFloat(item.price).toLocaleString("en-US", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
